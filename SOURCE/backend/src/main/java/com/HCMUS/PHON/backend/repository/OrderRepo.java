aa64e9f0cef1b39c06213710cfd81d72f8521206
Phon <ndphon23@clc.fitus.edu.vn>
2025-10-26 14:56:51 +0700

    get all my orders from Customer's account

--- SOURCE/backend/src/main/java/com/HCMUS/PHON/backend/repository/OrderRepo.java
+++ SOURCE/backend/src/main/java/com/HCMUS/PHON/backend/repository/OrderRepo.java
@@ -1,12 +1,10 @@
 package com.HCMUS.PHON.backend.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.HCMUS.PHON.backend.model.Cart;
 import com.HCMUS.PHON.backend.model.Order;
 import com.HCMUS.PHON.backend.model.Users;
 
